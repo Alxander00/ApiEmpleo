822f9e9db97691ef47debdb9fa23544499a8bcc1
Alxander00 <alextejada025@gmail.com>
2026-04-17 21:33:12 -0600

    Estructura base del API

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import express, { json } from 'express';
+import dotenv from 'dotenv';
+import { pool } from './db.js';
+
+import authRoutes from './routes/auth.js';
+import candidatosRoutes from './routes/candidato.js';
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.use('/api', authRoutes);
+app.use('/api/candidatos', candidatosRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Servidor funcionando.');
+})
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Servidor corriendo en http://localhost:${process.env.PORT || 3000}`);
+})
