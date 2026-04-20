11881a23e3ac8171bba25ef7de0f56d0b2510272
CarlosMojic <stevenmojica1gc@gmail.com>
2026-04-19 19:41:30 -0600

    feat: módulo de categorías (listado público y por id)

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import authRoutes from './routes/auth.js';
 import candidatosRoutes from './routes/candidato.js';
 import vacantesRoutes from './routes/vacantes.js';
 import adminRoutes from './routes/admin.routes.js';
+import categoriasRoutes from './routes/categorias.routes.js';
 
 const app = express(); 
 app.use(express.json());
@@ -16,7 +17,8 @@ app.use(express.json());
 app.use('/api', authRoutes);
 app.use('/api/candidatos', candidatosRoutes);
 app.use('/api/vacantes', vacantesRoutes);
-app.use('/api/admin', adminRoutes); // 👈 AQUÍ VA
+app.use('/api/admin', adminRoutes);
+app.use('/api/categorias', categoriasRoutes);
 
 // Ruta base
 app.get('/', (req, res) => {
