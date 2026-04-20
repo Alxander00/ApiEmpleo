fc0a840a7cb96e2d8b1f5f9cbedf0f7dce379f17
Alxander00 <alextejada025@gmail.com>
2026-04-19 22:35:31 -0600

    Modulo de postulaciones integrado y limpio

--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 import dotenv from 'dotenv';
-dotenv.config({ path: './.env' }); // 👈 PRIMERO
+dotenv.config({ path: './.env' }); // PRIMERO
 
 import express from 'express';
 import { pool } from './db.js';
 
-
-
-
-
-
-import postulacionesRoutes from './routes/postulaciones.js';
 import authRoutes from './routes/auth.js';
 import candidatosRoutes from './routes/candidato.js';
-import vacantesRoutes from './routes/vacantes.js';
 import empresasRoutes from './routes/empresas.routes.js';
+import vacantesRoutes from './routes/vacantes.js';
 import postulacionesRoutes from './routes/postulaciones.js';
-
 
 const app = express();
 app.use(express.json());
@@ -23,8 +16,9 @@ app.use(express.json());
 // Rutas
 app.use('/api', authRoutes);
 app.use('/api/candidatos', candidatosRoutes);
+app.use('/api/empresas', empresasRoutes);
+app.use('/api/vacantes', vacantesRoutes);
 app.use('/api/postulaciones', postulacionesRoutes);
-
 
 // Ruta base
 app.get('/', (req, res) => {
